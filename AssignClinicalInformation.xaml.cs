349d8f2063e1efdb26a9c0fafd6f1933f2fa8479
danny-moczynski <dannymoczynski4@gmail.com>
2023-10-18 18:36:38 -0500

    Added 3 xaml pages with functionability. Starting point.

---
+++ AssignClinicalInformation.xaml.cs
@@ -0,0 +1,73 @@
+namespace ClinicalCoordinationApplication;
+
+public partial class AssignClinicalInformation : ContentPage
+{
+    public AssignClinicalInformation()
+    {
+        InitializeComponent();
+    }
+        private void SideMenuButton_Clicked(object sender, EventArgs e)
+        {
+            // Handle the side menu button click
+            // You can add your code to open the side menu or perform any other action here
+        }
+
+        private void ProfileIcon_Clicked(object sender, EventArgs e)
+        {
+            // Handle the profile icon button click
+            // You can add your code to navigate to the user's profile or perform any other action here
+        }
+
+        private void OnNewNoteButtonClicked(object sender, EventArgs e)
+        {
+            var newNote = new Frame
+            {
+                CornerRadius = 10,
+                Padding = 5,
+            };
+
+            var editor = new Editor
+            {
+                Placeholder = "Enter your notes here...",
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+            };
+
+            newNote.Content = editor;
+
+            var deleteButton = new Button
+            {
+                Text = "Delete",
+                WidthRequest = 75,
+            };
+
+            deleteButton.Clicked += OnDeleteNoteButtonClicked;
+            deleteButton.IsVisible = true;
+
+            var noteStackLayout = new StackLayout
+            {
+                Orientation = StackOrientation.Horizontal,
+            };
+
+            noteStackLayout.Children.Add(newNote);
+            noteStackLayout.Children.Add(deleteButton);
+
+            NotesContainer.Children.Add(noteStackLayout);
+        }
+
+        private void OnDeleteNoteButtonClicked(object sender, EventArgs e)
+        {
+            // Get the "Delete" button that was clicked
+            var deleteButton = (Button)sender;
+
+            // Find the parent StackLayout which contains the note and delete button
+            var parentStackLayout = deleteButton.Parent as StackLayout;
+
+            if (parentStackLayout != null)
+            {
+                // Remove the parent StackLayout (the entire note with the delete button)
+                NotesContainer.Children.Remove(parentStackLayout);
+
+            }
+        }
+
+    }
--- CoordinatorDashboard.xaml.cs
+++ CoordinatorDashboard.xaml.cs
@@ -6,4 +6,30 @@ public partial class CoordinatorDashboard : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void CohortPicker_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        // Get the selected index
+        int selectedIndex = cohortPicker.SelectedIndex;
+
+        if (selectedIndex >= 0 && selectedIndex < cohortOptions.Length)
+        {
+            // Get the selected cohort from the array
+            var selectedCohort = cohortOptions[selectedIndex];
+        }
+    }
+
+    private string[] cohortOptions = { "October 2023", "Other Cohort Options..." };
+
+    private void SideMenuButton_Clicked(object sender, EventArgs e)
+    {
+        // Handle the side menu button click
+        // You can add your code to open the side menu or perform any other action here
+    }
+
+    private void ProfileIcon_Clicked(object sender, EventArgs e)
+    {
+        // Handle the profile icon button click
+        // You can add your code to navigate to the user's profile or perform any other action here
+    }
 }
