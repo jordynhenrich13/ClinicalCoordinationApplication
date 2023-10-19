c75345c634c322b5159fa7d0c7979e1bd0f78017
Olivia Ozbaki <ozbako81@uwosh.edu>
2023-10-18 20:33:09 -0500

    Added community toolkit.

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace ClinicalCoordinationApplication
 {
@@ -8,7 +9,7 @@ namespace ClinicalCoordinationApplication
         {
             var builder = MauiApp.CreateBuilder();
             builder
-                .UseMauiApp<App>()
+                .UseMauiApp<App>().UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
