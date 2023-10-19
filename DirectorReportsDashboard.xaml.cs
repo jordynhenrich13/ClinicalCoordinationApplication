b17928de5b6a507fabde1095f85309e82bef0801
Olivia Ozbaki <ozbako81@uwosh.edu>
2023-10-18 20:34:44 -0500

    Created director-view reports dashboard.

---
+++ DirectorReportsDashboard.xaml.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Maui.Views;
+
+namespace ClinicalCoordinationApplication;
+
+public partial class DirectorReportsDashboard : ContentPage
+{
+
+	public DirectorReportsDashboard()
+	{
+		InitializeComponent();
+
+	}
+
+    public void DisplayPopup()
+    {
+        var popup = new DirectorAddReportPopup();
+        this.ShowPopup(popup);
+    }
+
+    void AddReportClicked(object sender, EventArgs e)
+    {
+        DisplayPopup();
+    }
+}
+
