bc58351d01d13a6953762d1b203fdc3194bfb5cb
Olivia Ozbaki <ozbako81@uwosh.edu>
2023-10-18 20:36:05 -0500

    Created previous(ly used) clinics page.

---
+++ PreviousClinics.cs
@@ -0,0 +1,20 @@
+﻿
+
+using System.Collections.ObjectModel;
+
+namespace ClinicalCoordinationApplication;
+
+public partial class PreviousClinics : ContentPage
+{
+    //readonly List<string> previousClinicsQueried;
+    ObservableCollection<Clinic> PreviousClinicsQueried = new();
+
+    public PreviousClinics()
+	{
+		InitializeComponent();
+        PreviousClinicsQueried.Add(new Clinic("Aurora", 10));
+        PreviousClinicsQueried.Add(new Clinic("Thedacare", 20));
+        PreviousClinicsQueried.Add(new Clinic("SSM", 30));
+        PreviousClinicsList.ItemsSource = PreviousClinicsQueried;
+    }
+}
