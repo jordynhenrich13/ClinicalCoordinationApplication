56d204752893d748f7220c49c56177c8ae6c29af
Olivia Ozbaki <ozbako81@uwosh.edu>
2023-10-18 20:33:42 -0500

    Created mock class for clinic data in ListViews.

---
+++ Clinic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace ClinicalCoordinationApplication
+{
+    public interface IClinic
+    {
+        string ClinicName { get; }
+        int Distance { get; }
+    }
+
+    public class Clinic : IClinic
+    {
+        private readonly string _clinicName;
+        private readonly int _distance;
+
+        public Clinic(string clinicName, int distance)
+        {
+            _clinicName = clinicName;
+            _distance = distance;
+        }
+
+        public string ClinicName => _clinicName;
+
+        public int Distance => _distance;
+    }
+
+}
+
