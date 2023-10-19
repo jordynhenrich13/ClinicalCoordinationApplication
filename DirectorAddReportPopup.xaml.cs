d2d4eb3882d177709f63beccdef6690c5f0028d5
Olivia Ozbaki <ozbako81@uwosh.edu>
2023-10-18 20:35:16 -0500

    Created popup for director to add and send reports to coordinators.

---
+++ DirectorAddReportPopup.xaml.cs
@@ -0,0 +1,15 @@
+﻿using CommunityToolkit.Maui.Views;
+
+namespace ClinicalCoordinationApplication;
+
+public partial class DirectorAddReportPopup : Popup
+{
+	public DirectorAddReportPopup()
+	{
+        InitializeComponent();
+	}
+
+    /// NEED TO MAKE THESE DIFF? 
+    void OnSubmitButtonClicked(object sender, EventArgs e) => Close();
+    void OnCancelButtonClicked(object sender, EventArgs e) => Close();
+}
