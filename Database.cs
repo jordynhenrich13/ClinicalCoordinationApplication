fa09ad1d35a56c878c3825baacdb59b8d1b4fe31
Jordyn Henrich <henrij13@uwosh.edu>
2023-10-29 17:54:12 -0500

    Beginning of the Database class

---
+++ Database.cs
@@ -0,0 +1,50 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ClinicalCoordinationApplication;
+class Database
+{
+    private string connString;
+    // Constructor initializes and sets up the database.
+    // It also ensures that the file exists or creates it if not.
+
+    public Database()
+    {
+        connString = GetConnectionString();
+        CreateTables(connString);
+    }
+
+
+    // Builds a ConnectionString, which is used to connect to the database
+    static String GetConnectionString()
+    {
+        var connStringBuilder = new NpgsqlConnectionStringBuilder();
+        connStringBuilder.Host = "acid-mummy-13033.5xj.cockroachlabs.cloud";
+        connStringBuilder.Port = 26257;
+        connStringBuilder.SslMode = SslMode.VerifyFull;
+        connStringBuilder.Username = "jordyn"; // won't hardcode this in your app
+        connStringBuilder.Password = "zNJV69CZDF57UfwDZlHhQw"; // need to hardcode password
+        connStringBuilder.Database = "defaultdb";
+        connStringBuilder.ApplicationName = "whatever"; // ignored, apparently
+        connStringBuilder.IncludeErrorDetail = true;
+        return connStringBuilder.ConnectionString;
+    }
+
+    static void CreateTables(string connString)
+    {
+        using var conn = new NpgsqlConnection(connString); // a conn represents a connection to the database
+        conn.Open(); // open the connection ... now we are connected!
+        new NpgsqlCommand("CREATE TABLE IF NOT EXISTS ClinicalCoordinator (EmployeeID PRIMARY KEY, FirstName VARCHAR(25), LastName VARCHAR(50) Email VARCHAR(150), Username VARCHAR(50), Password VARCHAR(50), PhoneNumber VARCHAR(25)", conn).ExecuteNonQuery();
+        new NpgsqlCommand("CREATE TABLE IF NOT EXISTS Student (StudentID PRIMARY KEY, FirstName VARCHAR(25), LastName VARCHAR(50), Email VARCHAR(150), Username VARCHAR(50), Password VARCHAR(50), PhoneNumber VARCHAR(25), Address VARCHAR(100)", conn).ExecuteNonQuery();
+        new NpgsqlCommand("CREATE TABLE IF NOT EXISTS Clinic (ClinicID PRIMARY KEY, ClinicName VARCHAR(100), PrimaryAddress VARCHAR(25), ContactPerson VARCHAR(50), ContactEmail VARCHAR(150), ContactPhone VARCHAR(50)", conn).ExecuteNonQuery();
+        //new NpgsqlCommand("CREATE TABLE IF NOT EXISTS Clinic (ClinicID PRIMARY KEY, ClinicName  VARCHAR(100), PrimaryAddress VARCHAR(25), ContactPerson VARCHAR(50), ContactEmail VARCHAR(150), ContactPhone VARCHAR(50)", conn).ExecuteNonQuery();
+
+    }
+}
+
