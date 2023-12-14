f53d5b97475bf260f495d5ba28a8415fb9d2668e
Jordyn Henrich <henrij13@uwosh.edu>
2023-12-14 09:39:21 -0600

    Updates to student main page descriptions

--- CoordinatorReportsDashboard.xaml.cs
+++ CoordinatorReportsDashboard.xaml.cs
@@ -1,8 +1,8 @@
 ﻿using System.Collections.ObjectModel;
 using System.Text;
 using System.Threading;
-using AndroidX.Lifecycle;
-using AndroidX.Navigation;
+//using AndroidX.Lifecycle;
+//using AndroidX.Navigation;
 using ClinicalCoordinationApplication.Model;
 using ClinicalCoordinationApplication.Model.Reports;
 using CommunityToolkit.Maui.Alerts;
@@ -10,7 +10,7 @@ using CommunityToolkit.Maui.Core;
 using CommunityToolkit.Maui.Storage;
 using CommunityToolkit.Maui.Views;
 using Microsoft.Maui;
-using static Android.Icu.Text.AlphabeticIndex;
+//using static Android.Icu.Text.AlphabeticIndex;
 using Font = Microsoft.Maui.Font;
 
 namespace ClinicalCoordinationApplication;
